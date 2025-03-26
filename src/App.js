539686b28b838f7d468f3221ea6cd37adcbd80dd
Mihrac199 <mihrac.comart1996@gmail.com>
2025-03-27 01:01:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 
 function App() {
+
+  const step = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="steps">
+
+      <div className="numbers">
+
+        <div className={`${step >= 1 ? "active" : null}`}>1</div>
+        <div className={`${step >= 2 ? "active" : null}`}>2</div>
+        <div className={`${step >= 3 ? "active" : null}`}>3</div>
+
+      </div>
+
+      <div className="message">
+
+        <p>Step {step}: {messages[step - 1]}</p>
+
+      </div>
+
+
+      <div className="buttons">
+
+        <button className="btn">Prevıous</button>
+        <button className="btn">Next</button>
+
+      </div>
+
     </div>
+
   );
-}
+
+};
 
 export default App;
